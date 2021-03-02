63d5e147534c2afcaee8131eddafe356347690e2
Weston Jones <Weston Jones@SPACESTATION>
2021-03-02 14:20:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+
+loadDoc();
+function loadDoc() {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        // document.getElementById("demo").innerHTML = this.responseText;
+      //  var myArr = JSON.parse(this.responseText);
+          convertDoc(JSON.parse(this.responseText));
+      }
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhttp.send();
+  }
+  
+  function convertDoc(myUsers){
+      //build final json array for push
+      var customUsers = { applicant : "Weston Jones", users : []};
+  
+      //populate the customUsers array
+      for (var i = 0; i < myUsers.length; i++) 
+      {
+      var curUser = myUsers[i];
+  
+      //break the name into first and last
+      var full_name = curUser.name.split(" ");
+      var fName = full_name[0];
+      var lName =  full_name[1];
+      // create and push new object to final array
+      customUsers.users.push({
+          first_name:fName,
+          last_name:lName,
+          company_name:curUser.company.name,
+          company_full_address:(curUser.address.street + ", " + curUser.address.city + ", " + curUser.address.zipcode),
+          website: curUser.website,
+          phone:curUser.phone
+          })
+          
+      }
+      
+     
+  let data = JSON.stringify(customUsers);
+  const fs = require("fs");
+  fs.writeFileSync('Json.json', data);
+  
+    
+  
+  
+  // var testData = "hello";
+  // postData(testData);
+  
+      postWithShell();
+  }
+
+
+function postWithShell(){
+    const Shell = require('node-powershell');
+    const ps = new Shell({executionPolicy: 'Bypass',noProfile: true       });              
+    ps.addCommand('./sendJson.ps1');
+    ps.invoke()
+.then(output => {
+  console.log(output);
+})
+.catch(err => {
+  console.log(err);
+});
+}
+
+
+
+  function postData(data){
+    
+  // console.log(data);
+    fetch("https://ptsv2.com/t/eq55i-1614670001/post", {
+    method: 'POST', 
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    // ,
+    // body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  }
+  
---
+++ sendJson.ps1
@@ -0,0 +1,14 @@
+﻿
+$json = Get-Content -Raw -Path 'C:\Users\Weston Jones\source\repos\getRequest\jsonTester.json' | ConvertFrom-Json
+
+$newjson = $json | ConvertTo-Json -Compress
+ 
+# Initiate the POST request including the headers and the JSON payload
+#$response = Invoke-RestMethod 'https://scheduler.luminarycxm.com/api/v1/cleaned/data/test/' -Method Post -Body $newjson -ContentType 'application/json' -Headers $headers
+ 
+ $response = Invoke-RestMethod 'https://ptsv2.com/t/1psqp-1614718902/post' -Method Post -Body $newjson -ContentType 'application/json' -Headers $headers
+ 
+ 
+
+# Echo the response
+Write-Host $response
